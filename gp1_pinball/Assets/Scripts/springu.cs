3b18561dd5d0e3e6885a22b90cde1a8689b691de
moghmulla <mogh.mulla@gmail.com>
2025-01-06 21:53:23 +0300

    added stuff

--- gp1_pinball/Assets/Scripts/springu.cs
+++ gp1_pinball/Assets/Scripts/springu.cs
@@ -1,24 +1,59 @@
 // Copyright by #Team1 2025. All rights reserved. 
 
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class springu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	// components
+	Rigidbody2D body;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	Vector2 restPosition;
 
-   private void MoveSpring()
-   {
+	float firstLevel = -1f;
+	float secondLevel = -2f;
+	float thirdLevel = -3f;
 
-   }
+	[SerializeField] float addedVelocity = 0f;
+
+	 // Start is called once before the first execution of Update after the MonoBehaviour is created
+	 void Start()
+	 {
+		body = GetComponent<Rigidbody2D>();
+
+		restPosition = transform.position;
+	 }
+
+	 // Update is called once per frame
+	 void Update()
+	 {
+		MoveSpring();
+	 }
+
+	private void MoveSpring()
+	{
+		if ( Input.GetKey(KeyCode.Space) )
+		{
+			body.linearVelocity = new Vector2(0f, -1f);
+		}
+		else
+		{
+			body.linearVelocity = new Vector2(0f, addedVelocity);
+		}
+
+		if (restPosition.y - transform.position.y > firstLevel )
+		{
+			addedVelocity = -firstLevel;
+		}
+		if ( restPosition.y - transform.position.y > secondLevel )
+		{
+			addedVelocity = -secondLevel;
+		}
+		if (restPosition.y - transform.position.y > thirdLevel)
+		{
+			addedVelocity = -thirdLevel;
+		}
+
+	}
 
 }
